c23c0d5f5ffd0e40c0c53d33d526e5d9fde60db8
JackWang071 <35980971+JackWang071@users.noreply.github.com>
2020-12-06 23:34:14 -0800

    Parsing+decryption debugged
    Will test with more iterations + more threads

--- ECU.cpp
+++ ECU.cpp
@@ -8,17 +8,16 @@ int ECU::ID_vals = 0;
 
 ECU::ECU(Bus* new_bus) : CAN_Component::CAN_Component() {
 	ECU::bus = new_bus;
-	
 	ECU::bus->test_conn();
 	
 	ECU::msg_idx = 0;
 	ECU::bitclock = 0;
 	ECU::until_transmit_start = 0;
 	ECU::ECU_ID = ++ECU::ID_vals;
-	
 	ECU::ones_count = 0;
 	ECU::msg_ends.push_back(0);
 	
+	ECU::encrypt = true;
 }
 
 int ECU::test_conn(){
@@ -26,13 +25,8 @@ int ECU::test_conn(){
 	return 0;
 }
 
-/*
-{1, 1, 1, 0, 0, 1, 0, 1}
-{0, 1, 0, 0}
-*/
-
 // Interface to allow the bus to add a new bit to ECU's recvbuffer
-int ECU::recv_msg(bool nextbit){
+int ECU::recv_msg(int nextbit){
 	ECU::recv_buffer.push_back(nextbit);
 	
 	// Receiving a new bit from another ECU may cause this ECU to stop sending
@@ -41,15 +35,15 @@ int ECU::recv_msg(bool nextbit){
 		if(ECU::msg_idx < ECU::send_buffer.size()){
 			// If nextbit is 0 and the current bit being sent is 1, force transmission to pause for a while
 			// Theoretically should avoid the ECU from stopping transmission because of its own bits
-			if(nextbit < ECU::send_buffer[ECU::msg_idx]){
+			if(nextbit < (int)ECU::send_buffer[ECU::msg_idx] - 48){
 				ECU::until_transmit_start = ECU::bitclock + 120;
-				std::cout << "ECU-" << ECU::ECU_ID << " pauses." << std::endl;
+				std::cout << "\nECU-" << ECU::ECU_ID << " pauses." << std::endl;
 			}
 		}
 		// If this ECU is not sending but still receiving outside bits, then force transmission to pause for a while
 		else{
 			ECU::until_transmit_start = ECU::bitclock + 120;
-			std::cout << "ECU-" << ECU::ECU_ID << " pauses." << std::endl;
+			std::cout << "\nECU-" << ECU::ECU_ID << " pauses." << std::endl;
 		}
 	}
 	
@@ -73,24 +67,24 @@ int ECU::decrypt(int msg_begin, int msg_end){
 	// Extract the newest message from the receive buffer
 	std::string bitstream = "";
 	for(int i = msg_begin; i < msg_end; i++){
-		bitstream += std::to_string(ECU::recv_buffer[i]);
+		bitstream += (char) (ECU::recv_buffer[i] + 48);
 	}
-	
-	std::cout << "ECU-" << ECU::ECU_ID << " receives " << bitstream << std::endl;
 	
 	// Extract the data field
 	std::bitset<32> data64_32(bitstream.substr(19, 32));
 	std::bitset<32> data32_0(bitstream.substr(51, 32));
-	
-	std::cout << "ECU-" << ECU::ECU_ID << " parses data " << data64_32.to_string() + data32_0.to_string() << std::endl;
 		
 	// Decrypt the data field
 	uint32_t data_arr[2];
 	data_arr[0] = (uint32_t) data32_0.to_ulong();
 	data_arr[1] = (uint32_t) data64_32.to_ulong();
-	btea(data_arr, -2, ECU::teakey);
 	
-	std::cout<<"ECU-"<<ECU::ECU_ID<<" decrypts: "<<data_arr[1]<<" "<<data_arr[0]<<std::endl;
+	std::cout<<"\nECU-"<<ECU::ECU_ID<<" parses data "<<data_arr[1]<<" "<<data_arr[0];
+	
+	if(ECU::encrypt == true){
+		btea(data_arr, -2, ECU::teakey);
+		std::cout<<" and decrypts to " << data_arr[1] <<" "<<data_arr[0];
+	}
 }
 
 // Infinite loop to allow ECU to generate and send messages
@@ -106,47 +100,48 @@ int ECU::sending(){
 		 *	I have priority
 		 *	There are unsent bits in my message
 		 */
+		
 		if (ECU::msg_idx < ECU::send_buffer.size() && ECU::bitclock >= ECU::until_transmit_start){
-			ECU::bus->rebroadcast(ECU::send_buffer[ECU::msg_idx]);
 			std::cout << ECU::send_buffer[ECU::msg_idx];
+			ECU::bus->rebroadcast((int) ECU::send_buffer[ECU::msg_idx] - 48);
 			ECU::msg_idx++;
 		}
-		if (ECU::msg_idx >= ECU::send_buffer.size()) {
+		if (ECU::msg_idx == ECU::send_buffer.size()) {
+			std::cout<<std::endl;
 			msg_idx++;
 			num_messages++;
-			std::cout<<std::endl;
 		}
 		
 		// If not currently sending a message, generate a new message at random time intervals
 		if (ECU::msg_idx >= ECU::send_buffer.size() && rand() % 5 == 1){
-			ECU::send_buffer = generate_msg();
+			generate_msg();
 			ECU::msg_idx = 0;
 			
-			std::cout << "ECU-" << ECU::ECU_ID << " sends ";
+			std::cout << "ECU-" << ECU::ECU_ID << " << ";
 		}
 	}
 	return 0;
 }
 
 // Generate a random CAN data frame and encrypt the data field using XXTEA, if so specified.
-std::vector<bool> ECU::generate_msg(){
-	bool encrypt = true;
-	
+int ECU::generate_msg(){
 	// Generate two random unsigned integers for our data field
 	uint32_t data_arr[2];
 	data_arr[0] = (uint32_t) rand();
 	data_arr[1] = (uint32_t) rand();
 	
-	std::cout<< "ECU-" << ECU::ECU_ID << ": " <<data_arr[1]<<" "<<data_arr[0]<<std::endl;
-	
-	int arb_val = rand() % 30;
+	std::cout<< "ECU-" << ECU::ECU_ID << " generates " <<data_arr[1]<<" "<<data_arr[0];
 	
 	// If encrypt is true:
-	if(encrypt == true){
+	if(ECU::encrypt == true){
 		btea(data_arr, 2, ECU::teakey);
+		std::cout<< " and encrypts to " << data_arr[1]<<" "<<data_arr[0];
 	}
 	
+	std::cout<<std::endl;
+	
 	// Convert certain fields to bitset format
+	int arb_val = rand() % 30;
 	std::bitset<11> arb_bitset (arb_val);
 	std::bitset<4> dlc_bitset (8);
 	std::bitset<32> data32_0 (data_arr[0]);
@@ -154,34 +149,21 @@ std::vector<bool> ECU::generate_msg(){
 	std::bitset<64> data_bitset (data64_32.to_string() + data32_0.to_string());
 	std::bitset<15> crc_bitset (9999);
 	
-	std::cout<< "ECU-" << ECU::ECU_ID << ": " <<data_bitset <<std::endl;
+	// Concatenate these strings together into a data frame
+	ECU::send_buffer = "0" 						// Start of frame
+					+ arb_bitset.to_string() 	// 1:11 Identifier
+					+ '0'						// 12 Remote transmission request
+					+ '0'						// 13 Identifier extension bit
+					+ '0' 						// 14 Reserved bit
+					+ dlc_bitset.to_string() 	// 15:18 Data length code
+					+ data_bitset.to_string() 	// 19:[82] Data field
+					+ crc_bitset.to_string() 	// -25:-11 Cyclic redundancy check 
+					+ '0'						// -10 CRC delimiter
+					+ '1'						// -9 ACK slot
+					+ '1' 						// -8 ACK delimiter
+					+ "1111111";				// -7:-1 End of frame
 	
-	// Create a vector of booleans to represent this message
-	std::vector<bool> frame;
-	frame.push_back(0);	// Start of frame
-	for (int i = 0; i <= arb_bitset.size(); i++){	// 1:11 Identifier
-		frame.push_back(arb_bitset[i]);
-	}
-	frame.push_back(0);	// 12 Remote transmission request
-	frame.push_back(0);	// 13 Identifier extension bit
-	frame.push_back(0);	// 14 Reserved bit
-	for (int i = 0; i < dlc_bitset.size(); i++){	// 15:18 Data length code
-		frame.push_back(dlc_bitset[i]);
-	}
-	for (int i = 0; i < data_bitset.size(); i++){	// 19:[82] Data field
-		frame.push_back(data_bitset[i]);
-	}
-	for (int i = 0; i < crc_bitset.size(); i++){	// [82]:[96] Cyclic redundancy check 
-		frame.push_back(crc_bitset[i]);
-	}
-	frame.push_back(0);	// -10 CRC delimiter
-	frame.push_back(1);	// -9 ACK slot
-	frame.push_back(1);	// -8 ACK delimiter
-	for (int i = 0; i < 7; i++){		// -7:-1 End of frame
-		frame.push_back(1);
-	}
-	
-	return frame;
+	return 0;
 }
 
 #endif // !ECU_C
